9176c6e0a82be7ab81e6042fe898787b581aac16
Agusdeluca96 <Agus@MacBook-Air-de-Agustin.local>
2016-07-12 21:31:56 -0300

    RESET

--- web/reservation_cancel_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'conexion.php';
-	$query = "UPDATE reserva_propiedad SET estado = 3 WHERE (id_reserva_propiedad =  $_GET[id_reserva_propiedad])";
-    $result = mysql_query($query);
-    mysql_close($conexion);
-    header("Location:user_reservations_list.php");
-?>
